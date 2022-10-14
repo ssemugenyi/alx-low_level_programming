cb7da2d99e6b2003c7e9df5df633422d3ca4409e
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-14 16:10:49 +0000

    Single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
