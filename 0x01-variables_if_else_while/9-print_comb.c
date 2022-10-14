c38442f636b1ab8fabee62224b99ef2dbeebe122
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-14 20:17:27 +0000

    Printig combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch + '0');
+		if (ch != 9)
+		{
+			putchar(',');
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
