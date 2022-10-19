cb15c688e96b318634f5b111978b7e2e9c43e1ed
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-19 09:01:27 +0000

    Testing 0 case

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * positive_or_negative - Checks for positive or negative number
+  * @i: Always number
+  * Return: 0
+  */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("Is zero");
+	}
+}
