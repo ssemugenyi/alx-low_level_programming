b01effc746a0086f37cef5a2d2a5e3b916f14749
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-19 18:49:52 +0000

    Testing positive or negative fumction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
-  * positive_or_negative - Checks for positive or negative number
-  * @i: Always number
+  * main - Checks for positive or negative number
+  *
   * Return: 0
   */
-void positive_or_negative(int i)
+int main(void)
 {
-	i = 0;
+	int i = 0;
 
-	if (i == 0)
-	{
-		printf("%d is zero", i);
-	}
+	positive_or_negative(i);
+	return (0);
 }
