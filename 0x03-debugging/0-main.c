7a2f52632a76eb1c52e7cc35f8e3932a0d69b255
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-19 19:37:19 +0000

    Testing function with 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * main - Checks for positive or negative number
+  * main - tests function that prints if integer is positive or negative
   *
   * Return: 0
   */
@@ -10,5 +10,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
