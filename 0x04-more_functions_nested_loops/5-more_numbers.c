d682847efb769183e5f07cfbae76650ad7b243ff
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-20 20:34:59 +0000

    Printing more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * more_numbers - Prints more numbers
+  *
+  * Return: Nothing
+  */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
