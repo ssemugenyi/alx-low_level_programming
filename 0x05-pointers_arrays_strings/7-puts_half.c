70e882d9e4416effef3128405b68a83f0c4aab38
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-24 21:14:34 +0000

    Printing hals of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * puts_half - prints last half of the string
+  * @str: input string
+  * Return: Nothing
+  */
+void puts_half(char *str)
+{
+	int b = 0, i, n;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+	n = (b / 2);
+	if (n % 2 != 0)
+	{
+		n = ((b + 1) / 2);
+	}
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
