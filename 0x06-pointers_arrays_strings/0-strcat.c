cd45f17649e5196c0c048a476c2848c85a9f85ab
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-28 21:51:15 +0000

    String concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _strcat - a function that concentrates two strings
+  * @dest: destination
+  * @src: source
+  * Return: destination
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; (dest[i + j] = *src++) != '\0'; j++)
+	{
+	}
+	return (dest);
+}
