3c3f939fa3617e55ec29b4b938771de7642f9ce3
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-10-31 16:42:57 +0000

    Finding a character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
 	}
 	return (NULL);
