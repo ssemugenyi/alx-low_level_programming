9376a95ff23dea960f1b374379affc410f2b42e9
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-11-02 12:25:24 +0000

    Printing a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _puts_recursion - prints a string
+  * @s: string input
+  * Return: Nothing
+  */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
