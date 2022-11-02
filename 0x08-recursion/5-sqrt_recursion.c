e1eaae4d0d5ad7c8a629955de6fe2dc16228dba3
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-11-02 19:48:13 +0000

    Printing a squareroot of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+  * findsquareroot - my function to calculate sqroot
+  * @num: number
+  * @sqroot: store
+  * Return: square root
+  */
+int findsquareroot(int num, int sqroot)
+{
+	if (sqroot < 1)
+	{
+		return (-1);
+	}
+	else if (sqroot * sqroot == num)
+	{
+		return (sqroot);
+	}
+	else
+	{
+		return (findsquareroot(num, sqroot - 1));
+	}
+}
+/**
+  * _sqrt_recursion - funtion to print squareroot
+  * @n: number
+  * Return: square root
+  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return(findsquareroot(n, (n - 1) / 2));
+	}
+}
