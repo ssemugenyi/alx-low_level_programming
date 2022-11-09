0be663d3c7c0accca202f1e33f324499a0571d65
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-11-09 17:19:32 +0000

    A program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,22 +7,21 @@
   */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int i, sum = 0;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
+	if (argc < 1)
 		return (0);
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (!atoi(argv[i]))
 		{
-			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("%s\n", "Error");
+			return (1);
 		}
-		sum += atoi(argv[i]);
+		else
+		{
+			sum += atoi(argv[i]);
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
