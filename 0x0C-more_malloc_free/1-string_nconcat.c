90d8df1f80fb4c79c96df46772b656785af212d7
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-11-11 15:09:53 +0000

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,6 +37,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		num = _strlen(s2);
 	len = _strlen(s1) + num + 1;
 	ptr = malloc(sizeof(*ptr) * len);
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 	for (j = 0; j < num; j++)
