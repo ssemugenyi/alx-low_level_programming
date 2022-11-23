137d5f3cf9d975e842074b7024769b4d12627f02
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-11-23 21:58:36 +0000

    Calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+  * op_add - Adds
+  * @a: num one
+  * @b: num two
+  * Return: 0
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - performs subtraction
+  * @a: num one
+  * @b: num two
+  * Return: 0
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - performs multiplication
+  * @a: num one
+  * @b: num two
+  * Return: 0
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - performs division
+  * @a: num one
+  * @b: num two
+  * Return: 0
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - to find modulus
+  * @a: num one
+  * @b: num two
+  * Return: 0
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
