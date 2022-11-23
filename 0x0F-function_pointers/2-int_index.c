f565d88a2c7808f015ff7f371dc39111f574e974
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-11-23 21:10:34 +0000

    Searches an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,8 +19,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i] != 0))
-				return (i);
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
 	}
 	return (-1);
 }
