220cedee072e39a85d907f680dd4fea0e89e0d1e
ssemugenyi <ssemugenyiisaac2@gmail.com>
2022-11-23 14:12:50 +0000

    A program that prints the name of the fie it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * @argc: count
+  * @argv: arguments
+  * Return: o
+  */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
